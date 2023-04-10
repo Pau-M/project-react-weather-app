fbb875af961904c752a611fcfefcc70aefc876ef
Pau <mozga.paulina@gmail.com>
2023-04-10 22:07:05 +0200

    creating site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App project with React</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
